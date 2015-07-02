fec5dbcda5cd956592d7a8afc885f22b9175cb67
Katherine Osterried <katherine.osterried@env.ethz.ch>
2015-07-02 13:55:26 +0200

    Create the main.f90 file

---
+++ main.f90
@@ -0,0 +1,19 @@
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!  This is the main program of the model.
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+       program main
+
+       INTEGER x,y,z
+
+       x = 1.0
+       y = 1.0
+
+       z = x+y
+ 
+       PRINT *, 'The value of z is', z
+
+       end program main
+
+
+
